985e86df2e23ef890c01deea7ead7768cc6b4cbd
mzakiwidianto <mzakiwidianto@gmail.com>
2022-12-31 07:45:07 +0700

    pengumpulan modul3

--- MODUL3_ZAKI/src/Main.java
+++ MODUL3_ZAKI/src/Main.java
@@ -11,25 +11,25 @@ public class Main {
         do {
             System.out.println("==Menu Program==\n1. Square\n2. Circle\n3. Trapezoid\n0. Finish");
             try {
-                System.out.println("Select menu : ");
+                System.out.print("Select menu : ");
                 menu = input.nextInt();
                 switch (menu) {
                     case 1:
-                        System.out.println("Enter the length of side of the square : ");
+                        System.out.print("Enter the length of side of the square : ");
                         double side = input.nextDouble();
                         calculation.setSquare(side);
                         calculation.run();
                         System.out.println("The calculation result : " + calculation.getSquare());
                         break;
                     case 2:
-                        System.out.println("Enter the radius of the circle : ");
+                        System.out.print("Enter the radius of the circle : ");
                         double radius = input.nextDouble();
                         calculation.setCircle(radius);
                         calculation.run();
                         System.out.println("The calculation result : " + calculation.getCircle());
                         break;
                     case 3:
-                        System.out.println("Insert the side of the base of the trapezoid : ");
+                        System.out.print("Insert the side of the base of the trapezoid : ");
                         double a = input.nextDouble();
                         System.out.println("Enter the upper side of the trapezoid : ");
                         double b = input.nextDouble();
